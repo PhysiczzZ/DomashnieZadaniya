cfc749e444b728a6c1515c006ce098305e45d666
PhysiczzZ <kozyrev00@gmail.com>
2025-02-21 14:46:32 +0300

    Add files via upload

---
+++ Zadanie2.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int income = 44000, spending = 30000;
+        System.out.println("Итого (руб):");
+        System.out.print(income - spending);
+    }
+}
